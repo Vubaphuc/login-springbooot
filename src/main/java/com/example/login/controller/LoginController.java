39f3e4e2480aa5475aac5b9ed2793d4f9c333c0d
Vubaphuc <112857123+Vubaphuc@users.noreply.github.com>
2023-03-05 17:04:05 +0700

    update

---
+++ src/main/java/com/example/login/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.example.login.controller;
+
+
+import com.example.login.dto.UserDto;
+import com.example.login.request.LoginRequest;
+import com.example.login.service.LoginService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private LoginService service;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestParam() String userName, @RequestParam() String password) {
+        LoginRequest request = new LoginRequest();
+        request.setUserName(userName);
+        request.setPassword(password);
+        UserDto userDto = service.login(request);
+        return ResponseEntity.ok(userDto);
+    }
+
+}
